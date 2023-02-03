92c7e93289f561baf141a90bdc20c6f02e27128e
«anastasiatischenko» <1510anastasia@mail.ru>
2023-02-03 13:53:02 +0300

    Добавила все 3 задачи

---
+++ task10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!)!;
+int a = num / 100;
+int b = num % 10;
+int result = (num - a * 100 - b) / 10;
+Console.WriteLine($"Вторая цифра вашего числа - {result}.");
---
+++ task15/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+Console.Write("Введите число от 1 до 7: ");
+int num = int.Parse(Console.ReadLine()!)!;
+
+if (num <= 5)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Нет.");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Да.");
+}
